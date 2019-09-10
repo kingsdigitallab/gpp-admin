03d2cf6872624fbf4e49024fe04e3b881d542308
olgaloboda <olga.loboda.13@gmail.com>
2019-09-10 16:57:35 +0100

    added a generator of unique names for repeatable input fields, added delete and duplicate functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
+    // object for popups that appear when users hover over question marks 
     var additionalInfo = {'general-date-from': 'some information'};
 
-    // check if additional information for the field exists
+    // check if additional information for the field exists to display the question mark icon
     $('.additional-info-icon').get().forEach((el) => {
         var key = $(el).attr("data-content-type");
         if (!additionalInfo[key]) {
@@ -55,28 +56,99 @@ $(document).ready(function() {
 
 });
 
-function addField(el) {
-    $(el).before($(el).prev('.optional-to-delete').get(0).outerHTML);
+function updateAttribute(idToIncrement, name) {
+    var regex = new RegExp('(?<='+idToIncrement + '-)[0-9]+');
+    var name = name.replace(regex, (n) => {return ++n});
+    return name;
 }
 
-function deleteField(el) {
-    if ($(el).parent('.optional-to-delete').siblings('.optional-to-delete').length >= 1) {
-        $(el).parent('div').remove();
+function addField(el) {
+    var template = $(el).parent().prev().clone();
+    var idToIncrement = template.first().attr('data-content-reference');
+    if (idToIncrement) {
+        template.find('*[name]').each((i, elHasName) => {
+            if (elHasName.name && elHasName.name.includes(idToIncrement)) {
+                var newName = updateAttribute(idToIncrement, elHasName.name);
+                elHasName.name = newName;
+            }
+        })
     }
+    template.find("input[type=text], input[type=date], textarea").val("");
+    $(el).parent().before(template);
 }
 
 function addBlock(el) {
-    // $(el).parent().before($(el).parent().siblings('fieldset').get(0).outerHTML);
+    var template = $(el).parent().prev('fieldset').clone();
+    var idToIncrement = template.closest('fieldset').attr('data-content-reference');
+    if (idToIncrement) {
+        template.find('*[name]').each((i, elHasName) => {
+            if (elHasName.name && elHasName.name.includes(idToIncrement)) {
+                var newName = updateAttribute(idToIncrement, elHasName.name);
+                elHasName.name = newName;
+            }
+        })
+    }
+    template.find("input[type=text], input[type=date], textarea").val("");
+    $(el).parent().before(template);
 }
 
-function deleteBlock(el) {
-    $(el).parents('.optional-to-delete').first().remove();
+// update entity name part fields if the 'Royal name' checkbox is checked
+function updateBlock(el) {
+    var val= el.name.slice(0, -10);
+    if($(el).is(":checked")){
+        $(el).siblings('.required-fields').html(`
+            <div class="grid">
+                <span class="required">Forename(s)</span>
+                <input type="text" placeholder="Forename(s)" onfocus="this.placeholder=''" onblur="this.placeholder='Forename(s)'" value="" aria-label="forename" name="`+ val +`forename"/>
+            </div>
+            <div class="grid">
+                <span class="required">Proper title</span>
+                <input type="text" placeholder="Proper title" onfocus="this.placeholder=''" onblur="this.placeholder='Proper title'" value="" aria-label="proper title" name="`+ val +`proper-title"/>
+            </div>
+        `);
+    }
+    else if($(el).is(":not(:checked)")){
+        $(el).siblings('.required-fields').html(`
+            <div class="grid">
+                <span class="required">Surname</span>
+                <input type="text"  placeholder="Surname" onfocus="this.placeholder=''" onblur="this.placeholder='Surname'" value="" aria-label="surname" name="`+ val +`surname"/>
+            </div>
+        `);
+    }
 }
+
+// var warningPopup =`<div class="warningPopup">
+//                             <div class="modal-content">
+//                                 <div class="modal-header">
+//                                     <h2>Delete record?</h2>
+//                                     <input type="button" class="icon-only" aria-label="close" onclick="closeBlock('modal')" value="&#xf00d;" />
+//                                 </div>
+//                                 <div class="modal-body">
+//                                 </div>
+//                                 <div class="modal-footer">
+//                                     <input type="button" value="Cancel" class="default" onclick="closeBlock('modal')"/>
+//                                     <input type="submit" value="Delete" class="danger" onclick=""/>
+//                                 </div>
+//                             </div>
+//                         </div>`;
 
 function closeBlock(el) {
     $('.'+el).removeClass('active');
 }
 
-// add popups
+function deleteField(el) {
+    if ($(el).closest('.optional-to-delete').siblings('.optional-to-delete').length >= 1) {
+        $(el).closest('.optional-to-delete').remove();
+    }
+}
 
-// generate unique names for input fields
+function deleteBlock(el) {
+    $(el).parents('.optional-to-delete').first().remove();
+    $(el).parent().siblings('.optional-to-delete').each((i, parent) => {
+        console.log(parent);
+    });
+}
+
+function deleteRecord(el) {
+    console.log('delete record');
+}
